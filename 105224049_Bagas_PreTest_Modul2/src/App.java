5d3645b2478279ae7e09db0408a1633e6dd006d5
urbbaone <bagasefriliyantos@gmail.com>
2026-03-03 14:38:54 +0700

    first commit

---
+++ 105224049_Bagas_PreTest_Modul2/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("=========================================");
+        System.out.println("KARTU TANDA MAHASISWA");
+        System.out.println("Nama    : Bagas Efriliyanto Setiawan");
+        System.out.println("NIM     : 105224049");
+        System.out.println("Prodi   : Ilmu Komputer");
+        System.out.println("=========================================");    }
+}
